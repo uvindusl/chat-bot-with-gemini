debbbd853e0bab3b51f8d3286a69fe129667b15e
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-06-04 15:30:17 +0530

    changes in code

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ config = dotenv_values(".env")
 client = genai.Client(api_key=config['API_KEY'])
 
 
-def genarate_blog(paragraph_topic):
+def genarate(text):
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents=paragraph_topic,
+        contents=text,
         config=types.GenerateContentConfig(
             max_output_tokens=400,
             temperature=0.3
@@ -28,8 +28,8 @@ while keep_writing:
     # asking user do you want to continute the descusion
     answer = input('Do you want to help with gemini ? (Yes Enter Y, No Enter N) ')
     if (answer == 'Y'):
-        paragraph_topic = input('What do you want to talk about? ')
-        print(genarate_blog(paragraph_topic))
+        question = input('What do you want to talk about? ')
+        print(genarate(question))
     elif (answer == 'N'):
         print("Bye!")
         keep_writing = False
