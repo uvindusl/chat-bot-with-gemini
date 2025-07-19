6f2bb03257fba57e931477c18faad1b317583d0e
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-07-19 12:05:18 +0530

    remove some unwanted code

--- main.py
+++ main.py
@@ -51,60 +51,6 @@ def display():
 
     data = file.get('message')
 
-    # keep_writing = True
-    #
-    # while keep_writing:
-    #     while data != 'End':
-    #         answer = generateOutput(data)
-    #         logger.info(f"Sending data: {data}")
-    #         return jsonify({
-    #             "status": "success",
-    #             "message" : answer
-    #         }), 201
-    #
-    #         if data == 'End':
-    #             end = generateOutput("End")
-    #             break
-    #             return jsonify({
-    #                 "status": "success",
-    #                 "message" : end
-    #             }), 201
-    #
-    #         keep_writing = False
-
-
-# # get API Key from .env file
-# client = genai.Client(api_key=config['API_KEY'])
-#
-# def genarate(paragraph):
-#     response = client.models.generate_content(
-#         model="gemini-2.0-flash",
-#         contents=paragraph,
-#         config=types.GenerateContentConfig(
-#             system_instruction="You are a Robot. Your name is Jimmy.",
-#             max_output_tokens=400,
-#             temperature=0.3
-#         )
-#     )
-#
-#     retrieve_blog = response.text
-#     return retrieve_blog
-#
-# def display():
-#     keep_writing = True
-#
-#     while keep_writing:
-#         # asking user do you want to continue the discussion
-#         print('Jimmy the Robot at your service. How can I assist you today?')
-#         answer = input('if you want to chat with me simply type Start (If you want to end the chat simply type End) ')
-#         while answer != 'End':
-#             paragraph = input('What do you want to talk about? ')
-#             print(genarate(paragraph))
-#             if paragraph == 'End':
-#                 break
-#
-#         keep_writing = False
-
 @app.route('/input')
 def test():
     input_va = request.args.get('input')
